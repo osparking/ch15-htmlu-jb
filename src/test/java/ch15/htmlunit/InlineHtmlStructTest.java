6467cf06f184b5aea0b66a15576ce14bbe30ea96
osparking <jbpark03@gmail.com>
2023-12-30 21:23:59 +0900

    다중URL 한 연결에 지정, 제목 일치 확인

--- src/test/java/ch15/htmlunit/InlineHtmlStructTest.java
+++ src/test/java/ch15/htmlunit/InlineHtmlStructTest.java
@@ -3,6 +3,7 @@ package ch15.htmlunit;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.IOException;
+import java.net.URL;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,33 @@ public class InlineHtmlStructTest extends ManagedWebClient {
     webClient.setWebConnection(connection);
     HtmlPage page = webClient.getPage("http://page");
     WebAssert.assertTitleEquals(page, expectedTitle);
+  }
+
+  @Test
+  public void testInLineHtmlStructures() throws IOException {
+    final URL pageUrl1 = new URL("http://url1/");
+    final URL pageUrl2 = new URL("http://url2/");
+    final URL pageUrl3 = new URL("http://url3/");
+
+    MockWebConnection connection = new MockWebConnection();
+    String html1 = "<html><head><title>제목1</title></head></html>";
+    String html2 = "<html><head><title>제목2</title></head></html>";
+    String html3 = "<html><head><title>제목3</title></head></html>";
+
+    connection.setResponse(pageUrl1, html1.getBytes(UTF_8), 200, "OK",
+        "text/html; charset=UTF-8", null);
+    connection.setResponse(pageUrl2, html2.getBytes(UTF_8), 200, "OK",
+        "text/html; charset=UTF-8", null);
+    connection.setResponse(pageUrl3, html3.getBytes(UTF_8), 200, "OK",
+        "text/html; charset=UTF-8", null);
+
+    webClient.setWebConnection(connection);
+
+    HtmlPage page1 = webClient.getPage(pageUrl1);
+    WebAssert.assertTitleEquals(page1, "제목1");
+    HtmlPage page2 = webClient.getPage(pageUrl2);
+    WebAssert.assertTitleEquals(page2, "제목2");
+    HtmlPage page3 = webClient.getPage(pageUrl3);
+    WebAssert.assertTitleEquals(page3, "제목3");
   }
 }
