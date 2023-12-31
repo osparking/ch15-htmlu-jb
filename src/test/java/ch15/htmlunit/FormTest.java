6cecd7bfdda387677c17296ce71b1ca1f2f08821
osparking <jbpark03@gmail.com>
2023-12-31 14:55:28 +0900

    폼 입력 상자 값 입력 때 경보 비 발생 시험

--- src/test/java/ch15/htmlunit/FormTest.java
+++ src/test/java/ch15/htmlunit/FormTest.java
@@ -1,11 +1,13 @@
 package ch15.htmlunit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
@@ -16,6 +18,22 @@ import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class FormTest extends ManagedWebClient {
+  
+  @Test
+  @DisplayName("입력 값 제공 때 경보 없음")
+  public void testNoAlert() throws IOException {
+    CollectingAlertHandler handler = new CollectingAlertHandler();
+    webClient.setAlertHandler(handler);
+    
+    HtmlPage page = webClient.getPage("file:src/main/webapp/formtest.html");
+    HtmlForm form = page.getFormByName("validated_form");
+    HtmlTextInput input = form.getInputByName("in_text");
+    input.setValueAttribute("입력...");
+    HtmlSubmitInput submit = (HtmlSubmitInput) form.getInputByName("submit");
+    HtmlPage resultPage = submit.click();
+    WebAssert.assertTitleEquals(resultPage, "결과");
+    assertTrue(handler.getCollectedAlerts().isEmpty(), "예상 밖 경보 발생!");
+  }
 
   @Test
   public void testAlert() throws IOException {
