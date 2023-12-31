d49e59fe862ef6b8c42f10fb320d6dfcdb094e8c
osparking <jbpark03@gmail.com>
2023-12-31 11:51:57 +0900

    텍스트 입력 부재 때, 발생된 경보 시험

--- src/test/java/ch15/htmlunit/FormTest.java
+++ src/test/java/ch15/htmlunit/FormTest.java
@@ -1,9 +1,14 @@
 package ch15.htmlunit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
 import com.gargoylesoftware.htmlunit.WebAssert;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -12,6 +17,22 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class FormTest extends ManagedWebClient {
 
+  @Test
+  public void testAlert() throws IOException {
+    CollectingAlertHandler handler = new CollectingAlertHandler();
+    webClient.setAlertHandler(handler);
+    HtmlPage page = webClient.getPage("file:src/main/webapp/formtest.html");
+    HtmlForm form = page.getFormByName("validated_form");
+    HtmlSubmitInput button = form.getInputByName("submit");
+    HtmlPage result = button.click();
+    WebAssert.assertTitleEquals(result, page.getTitleText());
+    WebAssert.assertTextPresent(result, page.asNormalizedText());
+    
+    List<String> collectedAlerts = handler.getCollectedAlerts();
+    List<String> expectedAlerts = Collections.singletonList("값을 입력하세요.");
+    assertEquals(expectedAlerts, collectedAlerts);
+  }
+  
   @Test
   public void testForm() throws IOException {
     HtmlPage page = webClient.getPage("file:src/main/webapp/formtest.html");
